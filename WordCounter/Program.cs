75d001f4b33831959feb5c7f3e32e6e2b7508a36
Brayner <brayner.firth@gmail.com>
2014-01-08 00:13:27 +0000

    Technical Exercise
    1st Draft

---
+++ WordCounter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace WordCounter
+{
+    class Program
+    {
+        static void Main()
+        {
+
+
+            Console.WriteLine("UBS Technical Exercise – Fixed Income Developer position");
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("\n\nWrittten by Brayner Firth\n\n");
+
+            
+            do
+            {
+                Console.Write("\nInput: ");
+                
+                string sentence = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(sentence)) 
+                {
+                    break; // end if nothing is entered
+                }
+
+                // Simply Group by the words and exclude white spaces, blanks etc
+                var wordSplit= from word in sentence.ToLower().Split( new []{' ', ',', ';', '.','"','\n','\r'},StringSplitOptions.RemoveEmptyEntries)
+                          group word by word into w
+                          select new { Word = w.Key, Count = w.Count() };
+
+                
+                Console.WriteLine("Output: ");
+                foreach (var w in wordSplit)
+                {
+                    Console.WriteLine("{0} - {1}", w.Word, w.Count);
+                }    
+
+
+            } while (true);
+
+        }
+
+    }
+}
